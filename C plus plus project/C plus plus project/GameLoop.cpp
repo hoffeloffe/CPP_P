a94625685d554b3eb4f3ca0141b9e5c47a458022
Hoffe <djkagemand1@gmail.com>
2022-03-07 13:29:28 +0100

    GameLoop Class

---
+++ C plus plus project/C plus plus project/GameLoop.cpp
@@ -0,0 +1,68 @@
+#include "GameLoop.h"
+
+#include "GameOption.h"
+#include <string>
+#include <iostream>
+#include <Windows.h>
+#include <vector>
+#include "masterText.h"
+using namespace std;
+
+HANDLE color;
+string solution;
+string myGuess;
+string myGuess1;
+string myGuess2;
+string myGuess3;
+string myGuess4;
+masterText mBlue("Blue", 1);
+masterText mGreen("Green", 2);
+masterText mAqua("Aqua", 3);
+masterText mRed("Red", 4);
+masterText mPurple("Purple", 5);
+masterText mYellow("Yellow", 6);
+masterText mWhite("White", 7);
+masterText mGray("Gray", 8);
+
+vector<masterText> masterList = { mBlue, mGreen, mAqua, mRed, mPurple, mYellow, mWhite, mGray };
+
+void SetThatColor(string input)
+{
+	for (int i = 0; i < masterList.size(); i++)
+	{
+		if (masterList.at(i).name == input)
+		{
+			masterList.at(i).SetColor(color);
+		}
+	}
+}
+
+GameLoop::GameLoop()
+{
+}
+
+void GameLoop::EpicGameLoop()
+{
+	color = GetStdHandle(STD_OUTPUT_HANDLE);
+	cout << "\n" "Your game has startet \n";
+
+	solution = "Red Blue Green White";
+	cout << "The solution : " + solution + "\n";
+	cout << "Write your guess here: \n";
+
+	getline(cin, myGuess1);
+	SetThatColor(myGuess1);
+	getline(cin, myGuess2);
+	SetThatColor(myGuess2);
+	getline(cin, myGuess3);
+	SetThatColor(myGuess3);
+	getline(cin, myGuess4);
+	SetThatColor(myGuess4);
+
+	myGuess = myGuess1 + " " + myGuess2 + " " + myGuess3 + " " + myGuess4;
+
+	if (myGuess == solution)
+		cout << "Your the best\n";
+	else
+		cout << "You suck \n";
+}
--- C plus plus project/C plus plus project/GameOption.cpp
+++ C plus plus project/C plus plus project/GameOption.cpp
@@ -1,42 +1,11 @@
 #include "GameOption.h"
 #include <string>
 #include <iostream>
-#include <Windows.h>
-#include <vector>
-#include "masterText.h"
+#include "GameLoop.h"
 using namespace std;
-
-HANDLE color;
-string solution;
-string myGuess;
-string myGuess1;
-string myGuess2;
-string myGuess3;
-string myGuess4;
-masterText mBlue("Blue", 1);
-masterText mGreen("Green", 2);
-masterText mAqua("Aqua", 3);
-masterText mRed("Red", 4);
-masterText mPurple("Purple", 5);
-masterText mYellow("Yellow", 6);
-masterText mWhite("White", 7);
-masterText mGray("Gray", 8);
-
-vector<masterText> masterList = { mBlue, mGreen, mAqua, mRed, mPurple, mYellow, mWhite, mGray };
 
 GameOption::GameOption()
 {
-}
-
-void SetThatColor(string input)
-{
-	for (int i = 0; i < masterList.size(); i++)
-	{
-		if (masterList.at(i).name == input)
-		{
-			masterList.at(i).SetColor(color);
-		}
-	}
 }
 
 void GameOption::Clear()
@@ -46,7 +15,7 @@ void GameOption::Clear()
 
 void GameOption::Option()
 {
-	cout << "\n" "You have following options to pick (menu/restart) \n";
+	cout << "You have following options to pick (menu/restart) \n";
 
 	string choice;
 	getline(cin, choice);
@@ -86,31 +55,10 @@ void GameOption::Restart()
 void GameOption::StartGame()
 {
 	bool playing = true;
+	GameLoop gameloop;
 	while (playing)
 	{
-		color = GetStdHandle(STD_OUTPUT_HANDLE);
-		cout << "\n" "Your game has startet \n";
-
-		solution = "Red Blue Green White";
-		cout << "The solution : " + solution + "\n";
-		cout << "Write your guess here: \n";
-
-		getline(cin, myGuess1);
-		SetThatColor(myGuess1);
-		getline(cin, myGuess2);
-		SetThatColor(myGuess2);
-		getline(cin, myGuess3);
-		SetThatColor(myGuess3);
-		getline(cin, myGuess4);
-		SetThatColor(myGuess4);
-
-		myGuess = myGuess1 + " " + myGuess2 + " " + myGuess3 + " " + myGuess4;
-
-		if (myGuess == solution)
-			cout << "Your the best\n";
-		else
-			cout << "You suck \n";
-
+		gameloop.EpicGameLoop();
 		cout << "1. Play again?\n";
 		cout << "2. Got to menu\n";
 		cout << "3. Got to option\n";
